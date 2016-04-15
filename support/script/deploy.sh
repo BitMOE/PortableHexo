d76defd3c6db1c7f21d7c8b6ee140f6db38770ae
Haowu Ge <ghw@7axu.com>
2016-04-15 08:03:11 -0400

    文字更新

--- support/script/deploy.sh
+++ support/script/deploy.sh
@@ -1,18 +1,26 @@
+#!/bin/env bash
 clear
-echo "HEXO辅助配置脚本"
-echo "Powered By Bitmoe"
 echo ""
-echo "进行这一步之前，请保证："
-echo "已经注册了Github并且新建了如 用户名.github.io 的仓库；"
+echo -ne "                     欢迎使用 HEXO Portable 辅助配置脚本\n\n"
+echo -ne "                          由 比特萌公司 创建并维护\n\n"
+echo "                       WebSite: https://www.bitmoe.com"
 echo ""
-echo "如果已经确认以上步骤，请按回车继续..."
+echo "在您执行本步骤之前，请您确认："
+echo ""
+echo "    1、您已经注册了 Github ；
+    2、新建一个以 您用户名开始，.github.io 结尾的仓库。
+        ➜ 例如我们的注册帐号为 Bitmoe , 需要创建名为 bitmoe.github.io 的仓库。"
+echo ""
+echo "如果您确认已完成以上步骤，请按 回车键 继续..."
+echo ""
 read TEMPINPUT
-echo "那好，请按照以下提示输入相关信息："
-echo "1、请输入你的Github的用户名，输入完毕回车确认！"
+echo "OK，根据提示信息，完成您的 HEXO Portable 初始化工作！"
+echo ""
+echo "第一步，请输入您的 Github 用户名并按下 回车键 确认以上操作！"
 read GITHUBID
-echo "2、请输入你的Github注册邮箱，输入完毕回车确认！"
+echo "第二步，请输入您的 Github 注册邮箱并按下 回车键 确认以上操作！"
 read GITHUBEMAIL
-echo "进行安装步骤，此步骤耗时较长，请耐心等待...."
+echo "正在执行 HEXO Portable 安装步骤，本步骤耗时较长，请您耐心等待..."
 
 git config --global credential.helper store
 git config --global user.name $GITHUBID
@@ -23,21 +31,32 @@ githubrepo="https://$GITHUBID@github.com/$GITHUBID/$GITHUBID.github.io.git"
 sed -i '/^  type:/d' _config.yml
 sed -i '/^  repo:/d' _config.yml
 sed -i '/^  branch:/d' _config.yml
-sed -i '/^  message:/d' _config.yml
+#sed -i '/^  message:/d' _config.yml
 
 sed -i '$a\  type: git' _config.yml
 sed -i '$a\  repo: '$githubrepo'' _config.yml
 sed -i '$a\  branch: master' _config.yml
-sed -i '$a\  message: 更新博客。' _config.yml
+#sed -i '$a\  message: 更新博客。' _config.yml
 
-echo "下面将进行部署测试，稍后将有一个openssh的对话框出现，请输入你的github用户密码。"
-echo "请按回车继续..."
+echo ""
+echo "                        HEXO Portable 即将执行部署测试，
+                   稍后系统将会弹出一个 OpenSSH 确认对话框，
+			请您输入您的 Github 账户密码。"
+echo ""
+echo "    请您按下 回车键 执行部署操作..."
 read TEMPINPUT
 
 hexo g
 hexo d
 
-echo "请访问 https://$GITHUBID.github.io 查看是否部署成功！"
-echo "HEXO 的初步配置完毕，更多的配置请参考官方文档~自带中文哦！"
-echo "本脚本由 Bitmoe Inc. 编写并提供技术支持！"
+echo "                      HEXO Portable 的初始化完毕，"
+echo "            请您访问 https://$GITHUBID.github.io 查看您的个人网站！！！"
+echo "               更多配置信息请参考官方文档~（自带中文哦！）"
+echo "         HEXO 官方简体中文文档：https://hexo.io/zh-cn/docs/"
+echo "         HEXO 官方繁体中文文档：https://hexo.io/zh-tw/docs/"
+echo ""
+echo "           感谢您对我们的支持，您的支持是我们继续走下去的原动力！"
+echo ""
+echo ""
+echo "请输入 “exit” 退出初始化界面。"
 read TEMPINPUT
